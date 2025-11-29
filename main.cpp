b7d047c714865210f82c071125a2447a0dc74d8a
Phillip Wagner <m0ssyblue7@gmail.com>
2025-11-29 12:09:46 -0800

    Start

---
+++ main.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include "IntBinaryTree.h"
+
+using namespace std;
+
+void loadFromFile(const string &filename, IntBinaryTree &tree) {
+    ifstream something(filename);
+    if (!infile){
+        cerr << "Error: Could not open file '" << filename << "'.\n";
+        return;
+    }
+    string code;
+    int count = 0
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+int main() {
+
+return 0;
+}
